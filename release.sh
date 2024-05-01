465044b493f03462f0e1017fadd7c832fc427bd0
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2024-05-01 23:03:50 +0300

    build: Remove pre-release

--- release.sh
+++ release.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-prerelease_type="$1"
-
 branch="$(git branch --show-current)"
 if [ "$branch" != "main" ] && [ "$branch" != "master" ]; then
   echo "Error: Releases must be created from trunk, run $0 in main/master."
@@ -13,9 +11,4 @@ if [ -n "$(git status -s)" ]; then
   exit 1
 fi
 
-if [ -n "$prerelease_type" ]; then
-  echo "Creating pre-release ($prerelease_type)"
-  pipx run --spec commitizen cz bump --changelog --prerelease "$prerelease_type"
-else
-  pipx run --spec commitizen cz bump --changelog
-fi
+pipx run --spec commitizen cz bump --changelog
